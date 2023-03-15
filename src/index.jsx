aa669aaffc3b8e8fd4f2bbc37b6985d026f4e870
Alisa <alisa.luoma@hotmail.fi>
2023-03-15 09:46:12 +0200

    Creating components

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Components/Layout/Header";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <Header />
+  </>
+);
